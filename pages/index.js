c362bfd7dd69bd6d119dcfbc1e027e16fe9203f2
3rfan <28968837+3rfaan@users.noreply.github.com>
2022-05-03 23:05:06 +0200

    Changes in fetching API

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,9 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get(NEXT_URL + "/api/products");
+  const res = await axios.get(
+    "https://nextjs-restaurant-app.vercel.app/api/products"
+  );
 
   return {
     props: {
